19e1f956d7f824ec490087cc1f4ba2c11116e01b
pds0309 <ehd0309@gmail.com>
2021-09-10 15:10:00 +0900

    Initial commit

---
+++ common/src/test/java/com/pds/common/CommonApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pds.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class CommonApplicationTest {
+    @Test
+    void contextLoads(){
+        // no
+    }
+}
