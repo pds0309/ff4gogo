a29ac88cd5c9cacdd4d872d419f83e39d79206b8
pds0309 <ehd0309@gmail.com>
2021-09-17 11:21:13 +0900

    [유저페이지] 유저조회 서비스,컨트롤러,페이지 추가
    닉네임으로 유저 조회하기 까지만 추가

---
+++ web/src/main/java/com/pds/web/controller/UserSearchController.java
@@ -0,0 +1,50 @@
+package com.pds.web.controller;
+
+
+import com.pds.common.dto.UsersDto;
+import com.pds.common.enums.Ranks;
+import com.pds.web.exception.ErrorInfo;
+import com.pds.web.exception.UserRequestException;
+import com.pds.web.utils.ResponseHandler;
+import com.pds.web.service.UserSearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+@Log4j2
+public class UserSearchController {
+    private final UserSearchService userSearchService;
+
+    @PostMapping("/users")
+    @ResponseBody
+    public ResponseEntity<UsersDto.Info> saveUser(@RequestBody(required = false) String nn) {
+        log.info(nn + "으로 접근 시도됨");
+        return ResponseHandler.generateResponse("유저 등록 성공",HttpStatus.OK , userSearchService.createUser(userSearchService.findUser(nn)));
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUser(Model model , @PathVariable String id){
+        try{
+            UsersDto.Info userDto = userSearchService.getUserInfo(id);
+            model.addAttribute("user",userDto);
+            model.addAttribute("rank", Ranks.getRanks(userDto.getHighRank()).getRankInfo());
+            return "searchuser";
+        }
+        catch (UserRequestException e){
+            model.addAttribute("errorcode",400);
+            model.addAttribute("errorname","BAD_REQUEST");
+            model.addAttribute("errormsg","["+e.getCode()+"]\n"+e.getMessage());
+            return "error";
+        }
+    }
+}
