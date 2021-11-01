4688c2a4144a6d5b4f442431766736bb6182d98d
pds0309 <ehd0309@gmail.com>
2021-11-01 09:54:15 +0900

    slapi 모듈 기대 득점 조회 기능 추가
    모듈만으로 리소스경로를 통해 독립적으로 조회가 가능하도록 구성

--- serverlessapi/src/main/java/com/pds/serverlessapi/api/XgApi.java
+++ serverlessapi/src/main/java/com/pds/serverlessapi/api/XgApi.java
@@ -2,10 +2,7 @@ package com.pds.serverlessapi.api;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
@@ -17,8 +14,19 @@ public class XgApi {
     private final RestTemplate restTemplate;
     private final HttpHeaders headers;
 
-    public String getXgVal(String shootInfo) throws HttpClientErrorException , HttpServerErrorException { ;
-        ResponseEntity<String> responseEntity = restTemplate.exchange("https://0o2ma3bzal.execute-api.ap-northeast-2.amazonaws.com/Prod/xgrf", HttpMethod.POST, new HttpEntity<>(shootInfo,headers), String.class);
-        return responseEntity.getBody();
+    private final String errorLog = "Xg API";
+
+    public XgDto getXgVal(String shootInfo){
+        try{
+            ResponseEntity<XgDto> responseEntity = restTemplate.exchange("https://0o2ma3bzal.execute-api.ap-northeast-2.amazonaws.com/Prod/xgrf", HttpMethod.POST, new HttpEntity<>(shootInfo,headers), XgDto.class);
+            return responseEntity.getBody();
+        }
+        catch(HttpClientErrorException | HttpServerErrorException e){
+            if (e.getRawStatusCode() == 429 || e.getRawStatusCode() == 403) {
+                throw new HttpClientErrorException(HttpStatus.FORBIDDEN , errorLog);
+            }  else {
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, errorLog);
+            }
+        }
     }
 }
