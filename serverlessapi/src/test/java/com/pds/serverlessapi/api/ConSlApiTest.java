96ece4cbedc1f138bd95b2c5192907f015e557a1
pds0309 <ehd0309@gmail.com>
2021-11-02 18:34:37 +0900

    슛 데이터들로 기대득점 리스트 응답 추가
    기존 /expectedgoal
    슛 데이터 1개로 요청 -> 해당하는 기대득점 수치 리턴
    
    추가 /expectedgoals
    슛 데이터 배열로 요청-> 모든 슛 데이터에 대한 기대득점 수치들 리턴

--- serverlessapi/src/test/java/com/pds/serverlessapi/api/ConSlApiTest.java
+++ serverlessapi/src/test/java/com/pds/serverlessapi/api/ConSlApiTest.java
@@ -1,6 +1,7 @@
 package com.pds.serverlessapi.api;
 
 import com.pds.serverlessapi.config.SlApiConfig;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +15,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +35,7 @@ class ConSlApiTest {
         assertNotNull(headers);
         assertNotNull(headers.get("x-api-key"));
     }
+
 
     @Test
     void conApiButNoBodyTest(){
@@ -58,4 +62,25 @@ class ConSlApiTest {
         JSONObject jsonObject = new JSONObject(responseEntity.getBody());
         assertTrue(jsonObject.has("prediction"));
     }
+
+    @Test
+    void conLogresListApiTest(){
+        String request = "{\n" +
+                "    \"inner\":\n" +
+                "    [\n" +
+                "    {\"assist\": 0, \"asx\": 0 , \"asy\": 0, \"fin\": \"1\", \"hed\": 0, \"inv\": 0, \"nom\": 0\n" +
+                ", \"x\": 0.88 ,\"y\": 0.45},\n" +
+                "{\"assist\": 0, \"asx\": 0 , \"asy\": 0, \"fin\": \"1\", \"hed\": 0, \"inv\": 0, \"nom\": 0\n" +
+                ", \"x\": 0.88 ,\"y\": 0.45 }\n" +
+                "    ]\n" +
+                "}";
+        HttpEntity<String> httpEntityHasBody = new HttpEntity<>(request, headers);
+        assertEquals(headers,httpEntityHasBody.getHeaders());
+        ResponseEntity<String> responseEntity = restTemplate.exchange("https://qgp7hgv778.execute-api.ap-northeast-2.amazonaws.com/Prod/xgrf", HttpMethod.POST, httpEntityHasBody, String.class);
+        assertEquals(200 , responseEntity.getStatusCodeValue());
+        JSONArray resultArray = new JSONArray(responseEntity.getBody());
+        assertEquals(2,resultArray.length());
+        assertTrue(resultArray.getJSONObject(0).has("prediction"));
+    }
+
 }
--- serverlessapi/src/test/java/com/pds/serverlessapi/controller/XgControllerTest.java
+++ serverlessapi/src/test/java/com/pds/serverlessapi/controller/XgControllerTest.java
@@ -1,26 +1,22 @@
 package com.pds.serverlessapi.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pds.serverlessapi.api.XgApi;
 import com.pds.serverlessapi.api.XgDto;
-import com.sun.net.httpserver.HttpServer;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.util.NestedServletException;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static org.mockito.BDDMockito.given;
@@ -45,13 +41,25 @@ class XgControllerTest {
 
 
     @Test
-    void getDetailFromMatchCodeTest() throws Exception {
-        given(xgApi.getXgVal("SOMETHING"))
+    void expectedGoalForShootTest() throws Exception {
+        given(xgApi.getXgVal("Shootinfo"))
                 .willReturn(xgDto);
         MvcResult mvcResult = mvc.perform(post("/expectedgoal")
-                .content("SOMETHING")
+                .content("Shootinfo")
                 .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
         assertEquals(0.25,new JSONObject(mvcResult.getResponse().getContentAsString()).getDouble("prediction"));
+    }
+    @Test
+    void expectedGoalsForShootsTest() throws Exception{
+        List<XgDto> xgDtoList = new ArrayList<>();
+        xgDtoList.add(xgDto);
+        xgDtoList.add(xgDto);
+        given(xgApi.getXgListVal("Shootinfo"))
+                .willReturn(xgDtoList);
+        mvc.perform(post("/expectedgoals")
+                .content("Shootinfo")
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
