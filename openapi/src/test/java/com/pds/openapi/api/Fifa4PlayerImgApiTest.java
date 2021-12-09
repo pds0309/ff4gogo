7aea12a49294607179571dc0d5c707444dc909d6
seok <76927397+pds0309@users.noreply.github.com>
2021-12-09 10:50:02 +0900

    Update Fifa4PlayerImgApiTest.java

--- openapi/src/test/java/com/pds/openapi/api/Fifa4PlayerImgApiTest.java
+++ openapi/src/test/java/com/pds/openapi/api/Fifa4PlayerImgApiTest.java
@@ -12,11 +12,6 @@ class Fifa4PlayerImgApiTest {
         assertEquals("https://fo4.dn.nexoncdn.co.kr/live/externalAssets/common/playersAction/p101000250.png", Fifa4PlayerImgApi.findPlayerImgUrl(101000250));
     }
     @Test
-    void noActionImgReturnTest(){
-        assertEquals("https://fo4.dn.nexoncdn.co.kr/live/externalAssets/common/players/p13743.png",Fifa4PlayerImgApi.findPlayerImgUrl(234013743));
-    }
-
-    @Test
     void noImageReturnTest(){
         assertEquals(Fifa4PlayerImgApi.DEFAULT_IMAGE,Fifa4PlayerImgApi.findPlayerImgUrl(111111111));
     }
